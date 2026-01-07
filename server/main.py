32cbef13d4be78cf61f053142e8430a2bd26ad32
smypmsa <antonsauchyk@gmail.com>
2026-01-07 22:50:45 +0000

    fix: load env in core

--- server/main.py
+++ server/main.py
@@ -2,7 +2,10 @@
 
 from contextlib import asynccontextmanager
 
+from dotenv import load_dotenv
 from fastapi import FastAPI
+
+load_dotenv()  # Load .env file
 from fastapi.middleware.cors import CORSMiddleware
 from loguru import logger
 
